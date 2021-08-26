58c38b1013e3215ca85102373488ff7537360cdd
liplum <Li_plum@outlook.com>
2021-08-26 23:26:52 +0800

    Resource

---
+++ Bang/IO/ResLocation.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bang.Core;
+public class ResLocation
+{
+    public ResLocation([NotNull] string @namespace, [NotNull] string path, [NotNull] char separator = '\\')
+    {
+        Namespace = @namespace;
+        Path = path;
+        Separator = separator;
+        PathParts = Path.Split(separator);
+    }
+
+    public string Namespace
+    {
+        get; init;
+    }
+
+    public string Path
+    {
+        get; init;
+    }
+
+    public string[] PathParts
+    {
+        get; init;
+    }
+
+    public char Separator
+    {
+        get; init;
+    }
+}
