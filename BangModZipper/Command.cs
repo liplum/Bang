9c2d5a52456e8bdbc76d190e30cd9f2f0bdf50da
Li Plum <Li_plum@outlook.com>
2021-09-05 16:55:29 +0800

    Command

---
+++ BangModZipper/Command.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using static BangModZipper.ICommand;
+
+namespace BangModZipper;
+public interface ICommand
+{
+    public string Identify
+    {
+        get;
+    }
+
+    public string Name { get; }
+
+    public string Description { get; }
+
+    public void Execute(string[] args);
+
+    public delegate void ExecuteBehavior(string[] args);
+}
+
+public class Command : ICommand
+{
+    public Command([NotNull] string identify, [NotNull] string name, [NotNull] string description, [AllowNull] ExecuteBehavior behavior = null)
+    {
+        Identify = identify;
+        Name = name;
+        Description = description;
+        Behavior = behavior;
+    }
+
+    public ExecuteBehavior? Behavior { get; init; }
+
+    public string Identify
+    {
+        get; init;
+    }
+
+    public string Name
+    {
+        get; init;
+    }
+
+    public string Description
+    {
+        get; init;
+    }
+
+    public virtual void Execute(string[] args)
+    {
+        Behavior?.Invoke(args);
+    }
+}
---
+++ BangModZipper/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//zipper.exe -x <folder name> <mod name> <version>
+//-x:
+//  -p:Makes a mod pack.
+//  -i:Generates a mod info file.
+Console.WriteLine("Hello, World!");
